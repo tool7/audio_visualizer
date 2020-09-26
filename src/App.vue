61e965829bd0cbb9caec33d20248d70dc5663cbb
tool7 <marko.roje@gmail.com>
2020-09-26 17:51:44 +0200

    Init Vue and Element UI

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <audio-drag-drop />
+    <br />
+    <el-button @click="onClick">Button</el-button>
+  </div>
+</template>
+
+<script>
+import AudioDragDrop from "./components/AudioDragDrop.vue"
+
+export default {
+  name: "App",
+  components: {
+    AudioDragDrop,
+  },
+  methods: {
+    onClick() {
+      alert("test")
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
